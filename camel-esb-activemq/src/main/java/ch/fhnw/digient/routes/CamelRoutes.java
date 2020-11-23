002956d71d74de50f4708471aa7458676cc05260
charutapande <charutapande@gmail.com>
2020-11-23 15:54:54 +0100

    Update CamelRoutes.java

--- camel-esb-activemq/src/main/java/ch/fhnw/digient/routes/CamelRoutes.java
+++ camel-esb-activemq/src/main/java/ch/fhnw/digient/routes/CamelRoutes.java
@@ -11,14 +11,20 @@ import ch.fhnw.digient.model.CaseJSON;
 import ch.fhnw.digient.model.CaseXML;
 import ch.fhnw.digient.processor.CamelProcessor;
 
+/**
+ * @author charuta.pande
+ * This class is used to define the routing rules
+ * A route contains the integration logic and is implemented using EIP
+ *
+ */
+
 @Component
 public class CamelRoutes extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
 		// XML Data Format
-		JaxbDataFormat xmlDataFormat = new JaxbDataFormat();
-		
+		JaxbDataFormat xmlDataFormat = new JaxbDataFormat();		
 		JAXBContext con = JAXBContext.newInstance(CaseXML.class);
 		xmlDataFormat.setContext(con);
 		 
@@ -26,10 +32,10 @@ public class CamelRoutes extends RouteBuilder {
 		JacksonDataFormat jsonDataFormat = new JacksonDataFormat(CaseJSON.class);
 
 		from("file:C:/caseFolder") //the folder from which files are read
-			.unmarshal(xmlDataFormat) //
-			.process(new CamelProcessor())
-			.marshal(jsonDataFormat)
-			.to("jms:queue:digient"); //consumer is the queue receiving messages
+			.unmarshal(xmlDataFormat) //convert the representation of the object used in communication to the internal representation
+			.process(new CamelProcessor()) //transform the data from XML to JSON
+			.marshal(jsonDataFormat) //convert the internal representation of the object to the representation needed for communication
+			.to("jms:queue:digient"); //send data to the queue as a message
 	}
 
 }
