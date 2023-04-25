476d3aff7379cb6aa3fb2e34152246f0a24a9da2
Marco Kaufmann <66012520+mahgoh@users.noreply.github.com>
2023-04-25 16:36:37 +0200

    fix: implement proper json format (#2)

--- camel-esb-activemq/src/main/java/ch/fhnw/digient/model/CaseJSON.java
+++ camel-esb-activemq/src/main/java/ch/fhnw/digient/model/CaseJSON.java
@@ -1,76 +1,41 @@
 package ch.fhnw.digient.model;
 
 public class CaseJSON {
-	private int problemCaseId;
-	private String problemCaseDescription;
-	private String problemCallerId;
-	private String problemCallDate;
-	private String problemCallDuration;
-	private String problemOperator;
-	private String problemClientName;
-	private String problemClientId;
+	private int incidentId;
+	private String shortDescription;
+	private String reportDateTime;
+	private String clientId;
 
-	public String getProblemCallerId() {
-		return problemCallerId;
+	public int getIncidentId() {
+		return incidentId;
 	}
 
-	public void setProblemCallerId(String problemCallerId) {
-		this.problemCallerId = problemCallerId;
+	public void setIncidentId(int incidentId) {
+		this.incidentId = incidentId;
 	}
 
-	public String getProblemCallDate() {
-		return problemCallDate;
+	public String getShortDescription() {
+		return shortDescription;
 	}
 
-	public void setProblemCallDate(String problemCallDate) {
-		this.problemCallDate = problemCallDate;
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
 	}
 
-	public String getProblemCallDuration() {
-		return problemCallDuration;
+	public String getReportDateTime() {
+		return reportDateTime;
 	}
 
-	public void setProblemCallDuration(String problemCallDuration) {
-		this.problemCallDuration = problemCallDuration;
+	public void setReportDateTime(String reportDateTime) {
+		this.reportDateTime = reportDateTime.substring(4);
 	}
 
-	public String getProblemOperator() {
-		return problemOperator;
+	public String getClientId() {
+		return clientId;
 	}
 
-	public void setProblemOperator(String problemOperator) {
-		this.problemOperator = problemOperator;
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
 	}
 
-	public String getProblemClientName() {
-		return problemClientName;
-	}
-
-	public void setProblemClientName(String problemClientName) {
-		this.problemClientName = problemClientName;
-	}
-
-	public String getProblemClientId() {
-		return problemClientId;
-	}
-
-	public void setProblemClientId(String problemClientId) {
-		this.problemClientId = problemClientId;
-	}
-
-	public int getProblemCaseId() {
-		return problemCaseId;
-	}
-
-	public void setProblemCaseId(int problemCaseId) {
-		this.problemCaseId = problemCaseId;
-	}
-
-	public String getProblemCaseDescription() {
-		return problemCaseDescription;
-	}
-
-	public void setProblemCaseDescription(String problemCaseDescription) {
-		this.problemCaseDescription = problemCaseDescription;
-	}
 }
